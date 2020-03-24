c3d4ce7c7b5316997ba59761dd3e6b0ce17ac86b
Ubuntu <ubuntu@ip-172-31-18-174.us-west-2.compute.internal>
2020-03-24 22:01:38 +0000

    updated index

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React, { Component } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import "./App.css"
-
-import Header from "./components/Header"
-import CatIndex from "./pages/CatIndex"
-import CatShow from "./pages/CatShow"
-
+import Header from './components/Header'
+import CatIndex from './pages/CatIndex'
+import CatShow from './pages/CatShow'
+import cats from './cats'
 class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      allCats: cats
+    }
+  }
   render(){
     return(
       <React.Fragment>
-        <Header />
+        <Header/>
         <Router>
           <Switch>
             <Route exact path="/cat/:id" component={ CatShow } />
-            <Route exact path="/" component={ CatIndex } />
+            <Route exact path="/" render={ (props) => <CatIndex cats={ this.state.allCats } /> } />
           </Switch>
         </Router>
       </React.Fragment>
     )
   }
 }
+
 export default App
--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { ListGroup, ListGroupItemHeading } from 'reactstrap';
 class CatIndex extends Component{
   render(){
     return(
@@ -7,6 +8,14 @@ class CatIndex extends Component{
       <ListGroupItemHeading>Cat One</ListGroupItemHeading>
       <ListGroupItemHeading>Cat Age- Cat Enjoys</ListGroupItemHeading>
       </ListGroup>
+      { this.props.cats.map((cat, index) => {
+  return(
+    <ListGroup key={ index }>
+      <h4>{ cat.name }</h4>
+      <small>{ cat.age } - { cat.enjoys }</small>
+    </ListGroup>
+    )
+  })}
       </React.Fragment>
     )
   }
